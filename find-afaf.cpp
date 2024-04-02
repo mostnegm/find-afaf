4db09f5a57b8dd25c3b1d6b2dca17e0eff6855e2
Mostafa <Mostafa@192.168.1.9>
2024-04-02 22:48:39 +0200

    Updated dialogue to test string manipulation

--- find-afaf.cpp
+++ find-afaf.cpp
@@ -4,45 +4,50 @@
 using namespace std;
 
 // State variables
-int chat_counters[7];
+int chat[7];
 int location;
 int step;
-string dialog=
-"LOC 0 CHAT 0 Hello how can I help you? /n"
-"LOC 0 CHAT 0 string of option 1 LOC = 0 /n"
-"LOC 0 CHAT 0 string of option 2 LOC = 1 /n"
+
+//LOC0=receptionist
+//LOC1=bench
+//LOC2=corridor
+//LOC3=manager office
+//LOC4=afaf office
+//LOC5=toilets
+//LOC6=prayers
+
+string dialog =
+"LOC 0 CHAT 0 موظف الاستقبال قالك شوية وجاية\n"
+"1- You wait for afaf on the bench ENTER 1 \n"
+"2- You refuse to wait and search for afaf ENTER 2\n"
+"LOC 1 CHAT 0 You've been waiting for 15 minutes and nothing happens.\n"
+"1- You go to the receptionist again\n"
+"2- You search for afaf\n"
+"LOC 2 CHAT 0 You find two office rooms and a prayer zone\n"
+"1- You enter first office \n"
+"2- You enter second office\n"
+"3- You enter prayer zone\n"
 ;
 
-string warnings=
-"STEP 5 thirty minutes left/n"
-"STEP 10 fifteen minutes left/n"
-"STEP 13 5 minutes left/n"
+string warnings =
+"STEP 5 thirty minutes left\n"
+"STEP 10 fifteen minutes left\n"
+"STEP 13 5 minutes left\n"
 ;
-
-/////////////////////////
-/* Game loop functions */
-/////////////////////////
-// This function runs the infinite game loop.
-// The game loop consists of:
-// - Checking the current game state.
-// - Updating the GUI based on the current game state (win, loose, next state).
-// - Waits for new user input.
-void startGame();
 
 //////////////////////
 /* Output functions */
 //////////////////////
+
 // Prints the provided string to the screen, with a slow animation.
-void printToNewLine(string text)
-{
-	// loop through each character in the text
-	for (size_t i = 0; i < text.size(); ++i)
-	{
-		// output one character
-		// flush to make sure the output is not delayed
-		cout << text[i]<<flush;
-		usleep(60000); // use Sleep on windows
-	}
+void printToNewLine(string text){
+    for (size_t i = 0; i < text.size(); ++i)
+    {
+        cout << text[i] << flush;
+        usleep(100000); // use Sleep on windows
+    }
+}
+    
 // Prints a short loading bar (. . .) simulating wait time, blocking input.
 void printWaitingToNewLine(int ms);
 
@@ -60,15 +65,48 @@ string findSelectedOptionLocUpdate(int loc, int chat, int option);
 ////////////////////////////
 /* State update functions */
 ////////////////////////////
+
 // Updates the current loc state using the next loc code.
 string updateCurrentLoc(string nextLoc);
-// Returns the proper warning message based on the current state, if any. 
-string checkIfWarningNeeded(int step);
+
+// Returns the proper warning message based on the current state, if any.
+void checkIfWarningNeeded(int step) {
+    if (step==5) {
+        cout << "60 minutes left";
+    }
+    if (step==10) {
+        cout << "30 minutes left";
+    }
+    if (step==13) {
+        cout << "15 minutes left";
+    }
+}
+
 // Returns whether the user has won the game.
 bool checkIfWin(int loc, int chat, int step);
 // Returns whether the user has lost the game.
-bool checkIfLoose(int loc, int chat, int step);
+bool checkIfLose(int loc, int chat, int step);
 
-int main() {
-    printToNewLine("You enter the corridor and find no one to talk to");
+
+/////////////////////////
+/* Game loop functions */
+/////////////////////////
+// This function runs the infinite game loop.
+// The game loop consists of:
+// - Checking the current game state.
+// - Updating the GUI based on the current game state (win, lose, next state).
+// - Waits for new user input.
+void startGame() {
+    while (true) {
+        printToNewLine(dialog.substr(0,5));
+        break;
+    }
+}
+
+
+
+int main()
+{
+    printToNewLine(dialog.substr(13,56));
+    cout<<endl;
 }
